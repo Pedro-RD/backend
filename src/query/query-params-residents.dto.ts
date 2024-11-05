60c9eca1901ed6ad3c44e211b056cc7fb89f499c
Pedro Dias <Pedro.Dias.T0098247@edu.atec.pt>
2024-11-05 19:45:06 +0000

    Fix bed number

--- src/query/query-params-residents.dto.ts
+++ src/query/query-params-residents.dto.ts
@@ -4,6 +4,6 @@ import { QueryParamsDto } from "./query-params.dto";
 export class QueryParamsResidentsDto extends QueryParamsDto{
   @IsOptional()
   @IsString()
-  @Matches(/^(id|bed|birthDate|name|fiscalId|nationality)$/)
+  @Matches(/^(id|bedNumber|birthDate|name|fiscalId|nationality)$/)
   orderBy?: string;
 }
