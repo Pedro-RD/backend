b020738c355db764c050751496c68c5c46a20025
Pedro Dias <Pedro.Dias.T0098247@edu.atec.pt>
2024-11-13 22:48:54 +0000

    asd allow

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
